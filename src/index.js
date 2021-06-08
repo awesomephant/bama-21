9b844ad76ecf80662fcbe9b0052845d9b10f2b2b
Max Kohler <max@koehler-kn.de>
2021-06-08 16:44:02 +0200

    Add about page

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ import { initFilters } from "./initFilters";
 import { initNav } from "./initNav";
 window.addEventListener("DOMContentLoaded", () => {
   const settingsEl = document.querySelector(".settings")
-
+  const images = document.querySelectorAll("img");
+  images.forEach((img) => {
+    if (img.complete) {
+      img.classList.add("loaded");
+      img.classList.add("cached");
+    }
+    img.addEventListener("load", () => {
+      img.classList.add("loaded");
+    });
+  });
   if (settingsEl) {
     initSettings()
     initLines()

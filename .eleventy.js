063b10cc165b91305e2c5f5f217bd9a0292197a2
Max Kohler <max@koehler-kn.de>
2021-06-22 16:43:35 +0200

    Support videos on project pages

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("projects", function (collectionApi) {
     return collectionApi.getFilteredByGlob("./projects/*.md");
   });
+  eleventyConfig.addFilter("cleanURL", function (url) {
+    return url.replace(/(https?:\/\/)|(www.)|(\/$)/g, "");
+  });
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("_redirects");
   eleventyConfig.addPassthroughCopy("favicon-16x16.png");

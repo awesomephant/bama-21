7bf9da2be24fabb0a411fd6b8516d3d5ee4399c8
Max Kohler <max@koehler-kn.de>
2021-07-06 15:10:22 +0200

    Hide draft projects with separate eleventy collection

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,7 @@ async function imageShortcode(src, alt, sizes, className) {
   src = "." + src;
   let widths = [400, 800, 1600, null]
   let formats = ["webp", "jpeg"]
+  if (!alt) alt = ""
   if (process.env.NODE_ENV === "dev") {
     widths = [null]
     formats = ["jpeg"]
@@ -25,7 +26,7 @@ async function imageShortcode(src, alt, sizes, className) {
     outputDir: "./_site/assets/images",
     urlPath: "/sose21/assets/images/"
   });
-  
+
   let imageAttributes = {
     alt,
     sizes,
@@ -45,6 +46,23 @@ module.exports = function (eleventyConfig) {
       else return 0;
     });
   });
+  eleventyConfig.addCollection("liveProjects", function (collectionApi) {
+    let liveProjects = []
+    const allProjects = collectionApi.getFilteredByGlob(["./projects/*.md"]).sort(function (a, b) {
+      if (a.data.title > b.data.title) return 1;
+      else if (a.data.title < b.data.title) return -1;
+      else return 0;
+    });
+
+    for (let i = 0; i < allProjects.length; i++) {
+      if (allProjects[i].data.draft === false) {
+        liveProjects.push(allProjects[i])
+      }
+    }
+
+    return liveProjects;
+
+  });
 
   eleventyConfig.addFilter("cleanURL", function (url) {
     return url.replace(/(https?:\/\/)|(www.)|(\/$)/g, "");
@@ -60,16 +78,16 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./css/");
   eleventyConfig.addWatchTarget("./src/");
   eleventyConfig.addPassthroughCopy({ "./admin/config.yml": "/admin/config.yml" });
-  if (process.env.NODE_ENV === "staging"){
+  if (process.env.NODE_ENV === "staging") {
     // We're using Netlify Large Media, which means we can't do our own image processing at build time.
     // Therefore we disable it in that environment, and pass along parameters for Netlify's image transform instead.
-    eleventyConfig.addLiquidShortcode("image", function(src, alt, sizes, className, parameters){
+    eleventyConfig.addLiquidShortcode("image", function (src, alt, sizes, className, parameters) {
       return `<img alt="${alt}" src="${src}?${parameters}" class="project__image loaded">`
     });
   } else {
     eleventyConfig.addLiquidShortcode("image", imageShortcode);
   }
-  
+
   eleventyConfig.addFilter("renderMarkdown", function (value) {
     return md.render(value);
   });
